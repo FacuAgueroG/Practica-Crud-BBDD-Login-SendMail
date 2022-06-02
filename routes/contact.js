99acee40b21639e76106dba294a39146fa40101b
Sixteensaltines13 <facuaguerog@gmail.com>
2022-06-02 13:40:48 -0300

    Cambios en el env para activar el envio de mail con mailtrap

--- routes/contact.js
+++ routes/contact.js
@@ -29,7 +29,8 @@ const transport = nodemailer.createTransport({
           pass: process.env.MAILTRAP_PASS,}
       });
 
-//let sendMailStatus = await transport.sendMail(emailMsg); 
+let sendMailStatus = await transport.sendMail(emailMsg); 
+
 res.redirect("/");
 
 
